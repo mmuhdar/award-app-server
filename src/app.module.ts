d15a2a2b3508873160615759d765ca9366a733a3
mmuhdar <mmuhdar17@gmail.com>
2023-04-19 11:17:02 +0800

    set resource profile and award

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AwardModule } from './award/award.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AwardModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
