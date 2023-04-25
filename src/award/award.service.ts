c3d25a7e002a1661f3c67ca1ccb39384632e9351
mmuhdar <mmuhdar17@gmail.com>
2023-04-25 19:01:29 +0800

    finish project

--- src/award/award.service.ts
+++ src/award/award.service.ts
@@ -4,6 +4,7 @@ import { errorHandler, excludeField } from 'src/utils';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Award } from '@prisma/client';
 import { TokenPayload } from 'src/profile/interface';
+import { AwardQuery } from './interface';
 
 @Injectable()
 export class AwardService {
@@ -27,13 +28,46 @@ export class AwardService {
     }
   }
 
-  async findAll(user: TokenPayload): Promise<Award[]> {
+  async findAll(user: TokenPayload, query: AwardQuery): Promise<Award[]> {
     try {
+      let data;
       const { id } = user;
-      const data = await this.prisma.award.findMany({
-        where: { profileId: id },
-        include: { owner: true },
-      });
+      let queryType;
+      let queryPoin;
+      if (Object.keys(query).length > 0) {
+        queryType = JSON.parse(query.type);
+        queryPoin = JSON.parse(query.poin);
+      }
+
+      if (!queryPoin && !queryType) {
+        data = await this.prisma.award.findMany({
+          where: { profileId: id },
+          include: { owner: true },
+        });
+      } else {
+        if (queryPoin[1] == 0) {
+          data = await this.prisma.award.findMany({
+            where: {
+              profileId: id,
+              AND: [
+                { type: { in: queryType } },
+                { poin: { gte: queryPoin[0] } },
+              ],
+            },
+          });
+        } else {
+          data = await this.prisma.award.findMany({
+            where: {
+              AND: [
+                { type: { in: queryType } },
+                { poin: { gte: queryPoin[0] } },
+                { poin: { lte: queryPoin[1] } },
+              ],
+            },
+          });
+        }
+      }
+
       data.forEach((el) => {
         excludeField(el.owner, ['createdAt', 'updateAt', 'role', 'email']);
       });
---
+++ src/award/interface/query.interface.ts
@@ -0,0 +1,4 @@
+export interface AwardQuery {
+  type: string;
+  poin: string;
+}
